248fd0e3efb5825e2d65fbf9975718f83351f7a5
joneill95 <124259968+joneill95@users.noreply.github.com>
2023-02-02 11:50:51 +0000

    Updated Account Class

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,11 +1,44 @@
 package model;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class Account {
+	public Account(String accountNumber, String userNameOfAccountHolder, String accountType, Date accountOpeningDate) {
+		super();
+		this.accountNumber = accountNumber;
+		this.userNameOfAccountHolder = userNameOfAccountHolder;
+		this.accountType = accountType;
+		this.accountOpeningDate = accountOpeningDate;
+	}
 	String accountNumber;
 	String userNameOfAccountHolder;
 	String accountType;
+	Date accountOpeningDate;
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getUserNameOfAccountHolder() {
+		return userNameOfAccountHolder;
+	}
+	public void setUserNameOfAccountHolder(String userNameOfAccountHolder) {
+		this.userNameOfAccountHolder = userNameOfAccountHolder;
+	}
+	public String getAccountType() {
+		return accountType;
+	}
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	public Date getAccountOpeningDate() {
+		return accountOpeningDate;
+	}
+	public void setAccountOpeningDate(Date accountOpeningDate) {
+		this.accountOpeningDate = accountOpeningDate;
+	}
 	
 }
